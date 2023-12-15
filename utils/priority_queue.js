210a3e6c0b9a7702f2bab5b1616af1a9cd367831
pgoq <97684729+pgoq@users.noreply.github.com>
2023-12-15 14:21:45 -0300

    fix priority queue

--- utils/priority_queue.js
+++ utils/priority_queue.js
@@ -1,33 +1,60 @@
 class PriorityQueue{
     constructor(){
-        this.arr = [];
+      this.heap = [0];
+      this.last = 1; 
     }
-
-    push(item){
-        if(this.isEmpty()){
-            this.arr.push(item);
-        }
-        
-        //O push está linear, tenho que trocar por uma heap depois
-        for(let i = 0; i < this.arr.length(); i++){
-            if(this.arr[i] < item){
-                this.arr.splice(i, 0, item);
-                break;
-            }
-        }
+  
+    push(val){
+      if(this.heap.length === this.last){
+        this.heap.push(0);
+      }
+      this.heap[this.last] = val;
+      this.last++;
+      this.bottomUp(this.last - 1);
     }
-
+  
     pop(){
-        if(this.isEmpty()) return "Underflow"
-        return this.arr.shift();
+      if(this.last > 1){
+        [this.heap[1], this.heap[this.last - 1]] = [this.heap[this.last - 1], this.heap[1]];
+        this.last--;
+        this.topDown(1);
+      }else{
+        console.assert(false, "Não pode dar pop em Priority Queue vazia");
+      }
     }
-
+  
     top(){
-        if(this.isEmpty()) return "Empty"
-        return this.arr[0];
+      if(this.last > 1){
+        return this.heap[1];
+      }else{
+        console.assert(false, "Não pode ver o topo de uma Priority Queue vazia");
+        return -1;
+      }
     }
-
-    isEmpty(){
-        return this.arr.length == 0;
+  
+    topDown(index){
+      const left = 2 * index;
+      const right = 2 * index + 1;
+  
+      if(left >= this.last || (this.heap[index] <= this.heap[left] && (right >= this.last || this.heap[index] <= this.heap[right]))){
+        return;
+      } 
+  
+      if(right < this.last && this.heap[right] < this.heap[left]){
+        [this.heap[index], this.heap[right]] = [this.heap[right], this.heap[index]];
+        this.topDown(right);
+      }else{
+        [this.heap[index], this.heap[left]] = [this.heap[left], this.heap[index]];
+        this.topDown(left);
+      }
     }
-}
+  
+    bottomUp(index){
+      if(index === 1) return;
+      if(this.heap[index] < this.heap[Math.floor(index / 2)]){
+        [this.heap[index], this.heap[Math.floor(index / 2)]] = [this.heap[Math.floor(index / 2)], this.heap[index]];
+        this.bottomUp(Math.floor(index / 2));
+      }
+    }
+  }
+  
