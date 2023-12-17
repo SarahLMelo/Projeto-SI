e3ca10ff79c058b9342122eb08ecde2b3b0ec1cb
SarahLMelo <61260788+SarahLMelo@users.noreply.github.com>
2023-12-17 13:03:23 -0300

    fix: fixed vis to show border

--- algorithms/bfs.js
+++ algorithms/bfs.js
@@ -4,23 +4,30 @@ class BFS extends Algorithm{
     }
 
     bfs(src){
+        /*
+        0 -> Not visited
+        1 -> Border
+        2 -> Visited
+         */
         let queue = []
-        let vis = Array(this.mat.rows).fill.map(() => Array(this.mat.columns).fill(false));
+        let vis = Array(this.mat.rows).fill.map(() => Array(this.mat.columns).fill(0));
         queue.push(src);
-        vis[src[0]][src[1]] = true;
+        vis[src[0]][src[1]] = 1;
 
         while(queue.length > 0){
             let cur = queue[0];
             queue.shift();
+
+            vis[cur[0]][cur[1]] = 2;
 
             for(let i = 0; i < 4; i++){
                 const x = cur[0] + this.dirX[i];
                 const y = cur[1] + this.dirY[i];
                 if(x >= 0 && x < this.mat.rows){
                     if(y >= 0 && y < this.mat.columns){
-                        if(vis[x][y] == true) continue;
+                        if(vis[x][y] != 0) continue;
                         queue.push([x, y]);
-                        vis[x][y] = true;
+                        vis[x][y] = 1;
                     }                                                                                                                                       
                 }
             }
